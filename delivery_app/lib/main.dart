6a5a44f170917dd17a8841612ed6461fa14efd57
melquetrindade <melquetrindade654@gmail.com>
2024-04-24 18:44:58 -0300

    criando o pageView dos banners de promoção

--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:delivery_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:delivery_app/widgets/slider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,51 +18,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white, 
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 10, top: 10, bottom: 5, right: 5),
-          child: Container(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(50.0),
-              child: Image.asset(
-                  "assets/img_perfil.png",
-                  height: 50.0,
-                  width: 10.0,
-                  fit: BoxFit.cover
-              ),
-            ),
-          ),
-        ),
-        centerTitle: true,
-        title: Text('Delivery App', 
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 14
-        ),),
-      ),
-      body: Column(
-        children: [
-          MySlider()
-        ],
-      )
     );
   }
 }
--- delivery_app/lib/widgets/slider.dart
+++ delivery_app/lib/widgets/slider.dart
@@ -13,31 +13,66 @@ class _MySliderState extends State<MySlider> {
     'https://img.freepik.com/premium-vector/food-delivery-banner-design-flat-design-online-order_42237-696.jpg',
     'https://img.freepik.com/premium-vector/food-delivery-online-mobile-phone-shopping-online-online-food-order-internet-ecommerce-concept-website-banner-3d-perspective-vector-illustration_473922-160.jpg',
   ];
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 200,
-          width: double.infinity,
-          child: PageView.builder(
-              itemCount: banners.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: SizedBox(
-                    height: 200,
-                    width: double.infinity,
-                    child: Image.network(
-                      banners[index],
-                      fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 200,
+            width: double.infinity,
+            child: PageView.builder(
+                onPageChanged: (index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                itemCount: banners.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(40),
+                      child: SizedBox(
+                        height: 200,
+                        width: double.infinity,
+                        child: Image.network(
+                          banners[index],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
-                  ),
-                );
-              }),
-        )
-      ],
+                  );
+                }),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [for (var i = 0; i < banners.length; i++)
+              buildIndicator(currentIndex == i)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildIndicator(bool isSelected) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 2),
+      child: Container(
+        height: isSelected ? 12 : 8,
+        width: isSelected ? 12 : 8,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: isSelected ? Colors.red : Colors.grey
+        ),
+      ),
     );
   }
 }
