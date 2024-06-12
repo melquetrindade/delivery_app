26e1772e66859bd11c6bc4ca3c69ba7d6c55e99c
melquetrindade <melquetrindade654@gmail.com>
2024-06-12 11:12:50 -0300

    iniciando a busca dos dados de vendas e pedidos do firebase para o site

--- delivery_admin/pages/vendas.js
+++ delivery_admin/pages/vendas.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
+import { getDocs, doc, collection} from 'firebase/firestore';
+import { db } from '../utils/firebase/firebaseService';
 
 export default function Vendas() {
+    const [vendas, setVendas] = useState([])
+    const [pedidos, setPedidos] = useState([])
+
+    const carregaVendas = async () => {
+        try{
+            var newData = []
+
+            const querySnapshot = await getDocs(collection(db, `loja/owner/vendas`));
+            querySnapshot.forEach((doc) => {
+                var docData = doc.data();
+                //console.log(docData)
+                newData.push(docData);
+            })
+
+            setVendas(newData)
+        } catch(error){
+            console.error('Erro ao adicionar dado:', error);
+            //openNotification({placement: 'topRight', title: 'ERRO', descricao: 'NÃO FOI POSSÍVEL CONTINUAR, TENTE NOVAMENTE!'})
+        }
+    }
+
+    const carregaPedidos = async () => {
+        try{
+            var newData = []
+
+            const querySnapshot = await getDocs(collection(db, `loja/owner/pedidos`));
+            querySnapshot.forEach((doc) => {
+                var docData = doc.data();
+                //console.log(docData)
+                newData.push(docData);
+            })
+
+            setVendas(newData)
+        } catch(error){
+            console.error('Erro ao adicionar dado:', error);
+            //openNotification({placement: 'topRight', title: 'ERRO', descricao: 'NÃO FOI POSSÍVEL CONTINUAR, TENTE NOVAMENTE!'})
+        }
+    }
+    
+    if(vendas.length != 0){
+        vendas.forEach((item) => {
+            console.log(item)
+        })
+    }
+    if(pedidos.length != 0){
+        vendas.forEach((item) => {
+            console.log(item)
+        })
+    }
+
     return(
-        <h1>Pág de vendas</h1>
+        <main>
+            <h1>Pág de vendas</h1>
+            <button onClick={carregaVendas}>Carregar Vendas</button>
+            <button onClick={carregaPedidos}>Carregar Pedidos</button>
+        </main>
+       
     );
 }
