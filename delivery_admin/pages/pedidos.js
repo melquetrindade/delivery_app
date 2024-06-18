ce37aa81ca5ef5b744b941cdffd93a496056e38d
melquetrindade <melquetrindade654@gmail.com>
2024-06-18 11:40:12 -0300

    iniciando o desenvolvimento da página de pedidos

--- delivery_admin/pages/pedidos.js
+++ delivery_admin/pages/pedidos.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { getDocs, collection} from 'firebase/firestore';
 import { db } from '../utils/firebase/firebaseService';
+import styles from '../styles/pedidos.module.css'
 
 export default function Pedidos() {
   const [pedidos, setPedidos] = useState([])
@@ -16,7 +17,7 @@ export default function Pedidos() {
               newData.push(docData);
           })
 
-          setVendas(newData)
+          setPedidos(newData)
       } catch(error){
           console.error('Erro ao adicionar dado:', error);
           //openNotification({placement: 'topRight', title: 'ERRO', descricao: 'NÃO FOI POSSÍVEL CONTINUAR, TENTE NOVAMENTE!'})
@@ -28,14 +29,45 @@ export default function Pedidos() {
     }, []);
 
     if(pedidos.length != 0){
-      vendas.forEach((item) => {
+      pedidos.forEach((item) => {
           console.log(item)
-    })
-  }
+      })
+    }
 
-    return(
-        <main>
-            <h1>Pág de pedidos</h1>
-        </main>
-    );
+    const showCarrinho = (idDropUp, idDropDown, idDiv) => {
+      const arrowDropUp = document.getElementById(idDropUp);
+      const arrowDropDown = document.getElementById(idDropDown);
+      const divCarrinho = document.getElementById(idDiv);
+
+      arrowDropUp.style.display = arrowDropUp.style.display === 'none' ? 'block' : 'none';
+      arrowDropDown.style.display = arrowDropDown.style.display === 'none' ? 'block' : 'none';
+      divCarrinho.style.display = arrowDropUp.style.display === 'none' ? 'none' : 'block';
+    }
+
+  return(
+      <main className={styles.main}>
+          <div className={styles.containerPedidos}>
+            {pedidos.map((pedido, index) => (
+              <div key={index}>
+                <div className={styles.contDivider}>
+                  <div>
+                    <p>{pedido.numPedido}</p>
+                    <p>{pedido.cliente}</p>
+                    <p>{pedido.data}</p>
+                  </div>
+                  <span class="material-symbols-outlined" id={`arrowDropUp-${index}`} style={{display: 'none'}} onClick={() => showCarrinho(`arrowDropUp-${index}`, `arrowDropDown-${index}`, `carrinho-${index}`)}>arrow_drop_up</span>
+                  <span className={`material-symbols-outlined ${styles.teste}`} id={`arrowDropDown-${index}`} onClick={() => showCarrinho(`arrowDropUp-${index}`, `arrowDropDown-${index}`, `carrinho-${index}`)}>arrow_drop_down</span>
+                </div>
+                <div className={styles.divCarrinho} id={`carrinho-${index}`}>
+                  <p>{`produto ${index}`}</p>
+                  <p>{`produto ${index}`}</p>
+                  <p>{`produto ${index}`}</p>
+                  <p>{`produto ${index}`}</p>
+                </div>
+              </div>
+            ))}
+            
+          </div>
+      </main>
+  );
 }
